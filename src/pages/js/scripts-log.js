300c5f217ad6b776cd4fef71c835494449ab7f85
Luana Paes <114548504+luanapaes@users.noreply.github.com>
2024-01-27 19:56:41 -0300

    feat: Aprimorar autenticação de campos antes do redirecionamento
    - Modifiquei as funções relacionadas à autenticação para validar os campos primeiro
      antes de redirecionar o usuário para outra página.

--- src/pages/js/scripts-log.js
+++ src/pages/js/scripts-log.js
@@ -1,7 +1,52 @@
 const form = document.getElementById('form-api');
-const emailUSer = document.getElementById('email').velue;
-const passwordUser = document.getElementById('password').velue;
+const massegeValid = document.querySelector('#valid-cadastro');
+const massege = massegeValid.querySelector('p');
 
-const url = 'https://fakestoreapi.com/auth/login';
+const url = 'http://44.211.238.104:3000/login';
 
 // função para o login
+form.addEventListener('submit', evento => {
+    evento.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const emailStandard = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (emailStandard.test(email) && password.length >= 8) {
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ email, password }),
+        })
+        .then(res => {
+            if (res.ok) {
+                return res.json();
+            } else {
+                throw new Error('Erro na autenticação');
+            }
+        })
+        .then(data => {
+            // se autenticação for bem-sucedida, redireciona para usuarios.html
+            window.location = 'usuarios.html';
+        })
+        .catch(error => {
+            // exibe a mensagem de erro em caso de falha na autenticação
+            massegeValid.style.display = "flex";
+            massegeValid.style.backgroundColor = '#ee483ca5';
+            massege.textContent = "Erro na autenticação. Verifique suas credenciais.";
+        });
+    }
+    else {
+        massegeValid.style.display = "flex";
+        massegeValid.style.backgroundColor = '#ee483ca5';
+        massege.textContent = "Informe email e senha válidos.";
+
+        // Aguarda 1s para recarregar a página
+        setTimeout(function () {
+            location.reload();
+        }, 1000); // 1s
+        return false; // Não envia o form
+    }
+});
--- src/pages/js/scripts.js
+++ src/pages/js/scripts.js
@@ -1,21 +1,61 @@
 // POST
 const form = document.getElementById('form-api');
-const url = 'http://localhost:3000/add'; // fake para teste
+
+const url = 'http://44.211.238.104:3000/add'; 
 
 form.addEventListener('submit', evento => {
     evento.preventDefault();
+
+    const validCadastro = document.querySelector('#valid-cadastro');
+    const message = validCadastro.querySelector('p');
+    const icon = validCadastro.querySelector('i');
 
     var name = document.getElementById('name').value;
     var lastname = document.getElementById('lastname').value;
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
+    let emailStandard = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
     if (emailStandard.test(email) && password.length >= 8) {
-        validCadastro.style.display = "flex";
+
+        const valuesInputs = {
+            firstName: name,
+            lastName: lastname,
+            email: email,
+            password: password
+        }
+
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(valuesInputs)
+        }).then(res => {
+            if (res.ok) {
+                return res.json();
+            } else {
+                throw new Error('Erro na autenticação');
+            }
+        })
+            .then(data => {
+                validCadastro.style.display = "flex";
+                setTimeout(function () {
+                    window.location = 'usuarios.html';
+                }, 1000);
+            })
+            .catch(error => {
+                // Exibe mensagem de erro em caso de falha na autenticação
+                validCadastro.style.display = "flex";
+                icon.style.display = 'none';
+                validCadastro.style.backgroundColor = '#ee483ca5';
+                message.style.textAlign = 'center'
+                message.textContent = "Erro na autenticação. Verifique suas credenciais.";
+            });
+
     }
     else if (!emailStandard.test(email) || password.length < 8 || name === '' || lastname === '') {
         const icon = validCadastro.querySelector('i');
-        const message = validCadastro.querySelector('p');
 
         validCadastro.style.display = "flex";
         validCadastro.style.backgroundColor = '#ee483ca5';
@@ -30,22 +70,6 @@ form.addEventListener('submit', evento => {
         }, 1000); // 1s
         return false; // não envia o form
     }
-
-    const valuesInputs = {
-        firstName: name,
-        lastName: lastname,
-        email: email,
-        password: password
-    }
-
-    fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(valuesInputs)
-    }).then(res => res.json()).then(data => console.log(data))
-
 })
 
 
