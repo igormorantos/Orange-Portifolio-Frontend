2a9a3b45396976dab83b60847b45776c251aa484
Igor Moreira <igor.moreira@lucastechnologyservice.com>
2024-01-29 23:37:22 -0300

    inserção da integração com banco de dados

--- src/pages/js/scripts-log.js
+++ src/pages/js/scripts-log.js
@@ -8,17 +8,31 @@ const url = 'https://orange-portifolio-api-a03fcc01e1a7.herokuapp.com/login';
 form.addEventListener('submit', evento => {
     evento.preventDefault();
 
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
+    const email = document.getElementById('email-adress').value;
+    const password = document.getElementById('user-password').value;
+/*
+    const fullName = document.getElementById('fullName').value;
+    const sub = document.getElementById('sub').value;
+    const family_name = document.getElementById('given_name').value;
+    const gEmail = document.getElementById('given_name').value;
+    const picture = document.getElementById('given_name').value;
+    const given_Name = document.getElementById('given_name').value;*/
+
     const emailStandard = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    if (emailStandard.test(email) && password.length >= 8) {
+    //if (emailStandard.test(email) && password.length >= 8) {
+        
+        const valuesInputs = {
+            email: email,
+            password: password
+        }
+        
         fetch(url, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ email, password }),
+            body: JSON.stringify(valuesInputs),
         })
         .then(res => {
             if (res.ok) {
@@ -35,9 +49,9 @@ form.addEventListener('submit', evento => {
             // exibe a mensagem de erro em caso de falha na autenticação
             massegeValid.style.display = "flex";
             massegeValid.style.backgroundColor = '#ee483ca5';
-            massege.textContent = "Erro na autenticação. Verifique suas credenciais.";
+            massege.textContent = error;
         });
-    }
+/*}
     else {
         massegeValid.style.display = "flex";
         massegeValid.style.backgroundColor = '#ee483ca5';
@@ -48,5 +62,41 @@ form.addEventListener('submit', evento => {
             location.reload();
         }, 1000); // 1s
         return false; // Não envia o form
-    }
+    }*/
 });
+
+
+
+
+    /*else if(fullName && sub && gEmail)
+    {
+        firstName = given_Name
+        lastName = family_name
+        password = sub
+        email = gEmail
+
+        fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ email, password }),
+        })
+        .then(res => {
+            if (res.ok) {
+                return res.json();
+            } else {
+                
+            }
+        })
+        .then(data => {
+            // se autenticação for bem-sucedida, redireciona para usuarios.html
+            window.location = 'usuarios.html';
+        })
+        .catch(error => {
+            // exibe a mensagem de erro em caso de falha na autenticação
+            massegeValid.style.display = "flex";
+            massegeValid.style.backgroundColor = '#ee483ca5';
+            massege.textContent = "Erro na autenticação. Verifique suas credenciais.";
+        });
+    }*/
