54b30357bbde18a304d44aa7891b456d39feac39
Luana Paes <114548504+luanapaes@users.noreply.github.com>
2024-01-31 22:45:42 -0300

    feat: estiliza os cards e aprimora funcionalidades
    adicionei uma funcionalidade ao botão da div info, agora ele abre o modal para cadastrar os projetos, e implementei a capacidade de excluir projetos da página.

--- src/pages/js/delecao.js
+++ src/pages/js/delecao.js
@@ -13,6 +13,7 @@ const btnFechar = document.getElementById('btn-fechar');
 
 // mostra a opção de excluir o card
 function confirmDelete(cardId) {
+    console.log(cardId)
     currentCardId = cardId;
     const confDelete = document.getElementById(`confirmDelete${cardId}`);
     confDelete.style.display = 'block'
@@ -26,13 +27,25 @@ function deleteSuccess(id) {
 
 // exclui o card do documento - deve excluir do db
 function deleteCard(id) {
+    console.log(id)
     const confDelete = document.getElementById(id);
 
     setTimeout(function () {
         confDelete.style.display = 'none';
-        document.getElementById(currentCardId).remove();
+        // document.getElementById(currentCardId).remove();
     }, 3000);
-    deleteSuccess(id);
+    // Obter projetos do localStorage
+    var projetos = JSON.parse(localStorage.getItem('projetos')) || [];
+    console.log(projetos)
+
+    // Filtrar o projeto com base no ID para removê-lo
+    projetos = projetos.filter(projeto => projeto.id !== id);
+
+    // Atualizar localStorage com a lista filtrada
+    localStorage.setItem('projetos', JSON.stringify(projetos));
+
+    // Remover o elemento do documento
+    document.getElementById(id).remove();
 }
 
 // não exclui o card 
